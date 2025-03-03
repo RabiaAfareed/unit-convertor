636c255f328b67ffbe0416356cb86a3c91e1b55a
unknown <rabiafareed07@gmail.com>
2025-03-03 21:19:46 +0500

    first commit

---
+++ script_run_context.py
@@ -0,0 +1,15 @@
+from typing import ForwardRef
+
+
+def enqueue(self, msg: ForwardRef) -> None:
+    """Enqueue a ForwardMsg for this context's session."""
+    if msg.HasField("page_config_changed") and not self._set_page_config_allowed:
+        raise StreamlitSetPageConfigMustBeFirstCommandError() # type: ignore
+
+    # We want to disallow set_page config if one of the following occurs:
+    # - set_page_config was called on this message
+    # - The script has already started and a different st call occurs (a delta)
+    if msg.HasField("page_config_changed") or (
+        msg.HasField("delta") and self._has_script_started
+    ):
+        self._set_page_config_allowed = False 
